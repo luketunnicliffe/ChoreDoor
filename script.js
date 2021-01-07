aa5bcf725d34dac5a398538fc2526d99545737b6
tekipakicoder <luke@eigowithluke.com>
2021-01-07 17:16:35 +0900

    Added robot image and first js

---
+++ script.js
@@ -0,0 +1,20 @@
+const doorImage1 = document.getElementById('door1');
+
+const botDoorPath = "./robot.svg";
+
+console.log("hi there");
+
+document.getElementById('hi').innerHTML = "This is from Javascript.";
+
+
+document.getElementById("demo").innerHTML = 5 + 10;
+
+
+
+doorImage1.onclick = () =>{
+
+doorImage1.src = "botDoorPath";
+
+console.log("hello");
+
+}
